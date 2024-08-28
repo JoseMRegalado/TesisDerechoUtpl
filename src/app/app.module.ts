37483105b138b02f40215c0dc6738acbe2acf092
JoseMRegalado <mjregalado2@utpl.edu.ec>
2024-08-27 23:40:17 -0500

    PrimerPaso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAfLxg29EMb18BgFdSasMJFoaiY83wLWy4",
+    authDomain: "tesisderechoutpl.firebaseapp.com",
+    projectId: "tesisderechoutpl",
+    storageBucket: "tesisderechoutpl.appspot.com",
+    messagingSenderId: "1051057023448",
+    appId: "1:1051057023448:web:230624b7a7cb2932956aad"
+  },
+};
