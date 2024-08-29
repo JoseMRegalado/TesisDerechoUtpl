e639a0eba4bdef348c6f84652e6e43de23b71ea5
JoseMRegalado <mjregalado2@utpl.edu.ec>
2024-08-29 09:44:11 -0500

    CambiosComponentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,25 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import {RouterModule, RouterOutlet, Routes} from "@angular/router";
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
+];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
+    RouterOutlet,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent],
